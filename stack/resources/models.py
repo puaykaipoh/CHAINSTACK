70035c1949e936a59d23de6b290a4b54e4f79a02
puaykai <puaykaipoh@gmail.com>
2019-04-09 20:15:34 +0800

    lock critical section to prevent create more resource than quota when multiuser

--- stack/resources/models.py
+++ stack/resources/models.py
@@ -1,6 +1,9 @@
 import uuid
+import threading
 from django.db import models
 from users.models import Profile
+
+lock = threading.Lock()
 
 # Create your models here.
 class Resource(models.Model):
@@ -8,7 +11,12 @@ class Resource(models.Model):
 	profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='resources')
 
 	def save(self, *args, **kwargs):
-		if self.profile.quota:
-			if self.profile.resources.count() >= self.profile.quota:
-				raise Exception('Number of resource more than quota') # TODO show nice message?
-		super(Resource, self).save(*args, **kwargs)
+		lock.acquire()
+		try:
+			if self.profile.quota:
+				if self.profile.resources.count() >= self.profile.quota:
+					raise Exception('Number of resource more than quota') # TODO show nice message?
+			super(Resource, self).save(*args, **kwargs)
+		except:
+			lock.release()
+		lock.release()
